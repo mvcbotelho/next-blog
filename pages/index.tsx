9bfcc329c591d7dd69fa42df193d296e5baa0afb
Marcus Botelho <marcus.itec@gmail.com>
2021-01-20 23:43:39 -0300

    Add about page and navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Header from '@components/header';
+import Header from "@components/header";
+import Navigation from "@components/navigation";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
+        <Navigation />
         <Header />
       </main>
     </>
