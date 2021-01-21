7675cc070c420aea21b1b440250dad8399ccd7d3
Marcus Botelho <marcus.itec@gmail.com>
2021-01-21 13:04:21 -0300

    Add post method

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import { InferGetStaticPropsType } from "next";
+import Link from "next/link";
 import Head from "next/head";
 
 import Header from "@components/header";
 import Navigation from "@components/navigation";
 
-export default function Home() {
+import { getPostList } from "@shared/utils";
+
+type PostList = string[];
+
+function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Head>
@@ -14,7 +20,29 @@ export default function Home() {
       <main>
         <Navigation />
         <Header />
+        {posts.length > 0 && (
+          <ul>
+            {posts.map((slug) => (
+              <li key={slug}>
+                <Link href={`post/${slug}`}>
+                  <a>{slug.replace("-", " ")}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </main>
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const posts: PostList = getPostList();
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default Home;
